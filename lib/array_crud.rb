69f67e349e8a875ec5067d78b9a27d55a7da947d
Tak H Kim <takhyun818@gmail.com>
2018-09-11 23:48:20 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,8 +19,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  x = ["array", "element"]
-  element = x.pop
+  x = ["elements", "arrays!"]
+  array = x.pop
 
 end
 
