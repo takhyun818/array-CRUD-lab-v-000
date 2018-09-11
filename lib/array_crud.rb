53ac04ccc8916713da29ea13eaa671ae6742bb3a
Tak H Kim <takhyun818@gmail.com>
2018-09-11 23:51:12 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  x =   ["1", "2", "3", "4"]
+  x =   ["wow", "2", "3", "4"]
   x.first
 end
 
